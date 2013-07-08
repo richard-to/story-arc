75e95d24060646fcfad73006691ed2651c2456dc
richard-to <gohn76@gmail.com>
2013-07-08 02:14:37 -0800

    feat(app): Add story list page. Integrate server data.

--- src/client/js/app/story/story.js
+++ src/client/js/app/story/story.js
@@ -1,27 +1,50 @@
-angular.module('story', [], function($routeProvider) {
-    $routeProvider.when('/story', {
+angular.module('storyServices', ['ngResource']).
+    factory('Story', function($resource) {
+        return $resource('/api/v1/stories');
+    });
+
+angular.module('story', ['storyServices'], function($routeProvider) {
+    $routeProvider.
+    when('/story', {
         templateUrl: '/js/app/story/story.tpl.html',
         controller: 'StoryCtrl'
+    }).
+    when('/story-list', {
+        templateUrl: '/js/app/story/story-list.tpl.html',
+        controller: 'StoryListCtrl'
     });
 });
 
-angular.module('story').controller('StoryCtrl', function($scope) {
-    $scope.storyTitle = "The story of bob";
-    $scope.pages = [
-        {content: "Bob crossed the road."},
-        {content: "He ate a hamburger."},
-        {content: "He went to sleep."}
-    ];
+angular.module('story').controller('StoryCtrl', function($scope, Story) {
+    $scope.story = {
+        title: "Untitled story",
+        author: "Anonymous",
+        pages: []
+    };
 
-    $scope.currentPage = $scope.pages[0];
+    $scope.currentPage = $scope.story.pages[0];
 
-    $scope.selectPage = function() {
+    $scope.select = function() {
         $scope.currentPage = this.page;
     };
 
-    $scope.addPage = function() {
+    $scope.add = function() {
         var page = {content: "Insert content here."};
-        $scope.pages.push(page);
+        $scope.story.pages.push(page);
         $scope.currentPage = page;
+    };
+
+    $scope.save = function() {
+        var story = new Story($scope.story);
+        story.$save(function(story, response) {
+            console.log(response);
+        });
+    };
+});
+
+angular.module('story').controller('StoryListCtrl', function($scope, $location, Story) {
+    $scope.stories = Story.query();
+    $scope.new = function() {
+        $location.path('/story');
     };
 });
